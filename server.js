628edd5b2706ef0096de68bd4f5583bea480c908
danielkorich <korich.daniel@gmail.com>
2021-01-08 13:56:51 +0200

    b3

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 const userRouter = require("./routes/users");
 const petRouter = require("./routes/pets");
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 const db = process.env.MONGO_URI;
@@ -39,5 +39,5 @@ app.use("/api/users", userRouter);
 app.use("/api/pets", petRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at ${port}`);
 });
